754b3eac5c2c1bc99014cb94140d92f55487f187
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-10-01 06:15:16 -0500

    v0.4.0 Option fecha desde hasta

--- nasa_cli/apod/app.py
+++ nasa_cli/apod/app.py
@@ -1,29 +1,35 @@
 """
 APOD: Astronomy Picture of the Day
 """
+from datetime import date, datetime, timedelta
+
 import rich
 import typer
 
 app = typer.Typer()
 
+HOY = date.today().strftime("%Y-%m-%d")
+
 
 @app.command()
-def bajar():
+def bajar(
+    fecha: str = typer.Option(HOY, help="Fecha de la foto"),
+):
     """Bajar el Astronomy Picture of the Day"""
-    rich.print("[green]Bajar el Astronomy Picture of the Day[/green]")
+    rich.print(f"[green]Bajar el Astronomy Picture of the Day[/green] de [blue]{fecha}[/blue]")
 
 
 @app.command()
-def bajar_rango():
+def bajar_rango(
+    desde: str = typer.Option(HOY, help="Fecha de inicio AAAA-MM-DD"),
+    hasta: str = typer.Option(HOY, help="Fecha de termino AAAA-MM-DD"),
+):
     """Bajar un rango de imagenes del Astronomy Picture of the Day"""
     rich.print("[green]Bajar un rango de imagenes Astronomy Picture of the Day[/green]")
 
-    # Definir muestra de datos
-    datos = [
-        {"fecha": "2021-01-01", "titulo": "Titulo 1", "url": "https://url1.com"},
-        {"fecha": "2021-01-02", "titulo": "Titulo 2", "url": "https://url2.com"},
-        {"fecha": "2021-01-03", "titulo": "Titulo 3", "url": "https://url3.com"},
-    ]
+    # Convertir las fechas a datetime
+    desde_dt = datetime.strptime(desde, "%Y-%m-%d")
+    hasta_dt = datetime.strptime(hasta, "%Y-%m-%d")
 
     # Elaborar tabla
     tabla = rich.table.Table()
@@ -31,8 +37,18 @@ def bajar_rango():
     tabla.add_column("Titulo", justify="left")
     tabla.add_column("Media", justify="left")
     tabla.add_column("URL", justify="left")
-    for item in datos:
-        tabla.add_row(item["fecha"], item["titulo"], "Imagen", item["url"])
+
+    # Agregar datos a la tabla
+    tiempo = desde_dt
+    while tiempo <= hasta_dt:
+        fecha = tiempo.strftime("%Y-%m-%d")
+        tabla.add_row(
+            fecha,
+            f"Foto del {fecha}",
+            "text/html",
+            f"https://noexiste.com/{fecha}.html",
+        )
+        tiempo += timedelta(days=1)
 
     # Mostrar tabla
     consola = rich.console.Console()
